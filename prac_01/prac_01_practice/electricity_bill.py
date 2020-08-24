22b3b6945c2c726bb0397efbf344812beec015af
Thomas-Wallis <thomas.walis@my.jcu.edu.au>
2020-08-24 16:10:50 +1000

    Changed program to use electricity Tariffs

--- prac_01/prac_01_practice/electricity_bill.py
+++ prac_01/prac_01_practice/electricity_bill.py
@@ -1,4 +1,14 @@
-hourly_price = int(input("Enter price in kWh: "))
+TARIFF_11 = 0.244618
+TARIFF_31 = 0.136928
+
+tariff = input("Which tariff? 11 or 31: ")
+while tariff != "11" and tariff != "31":
+    print("Please select a valid tariff")
+    tariff = input("Which tariff? 11 or 31: ")
+if tariff == "11":
+    hourly_price = TARIFF_11
+else:
+    hourly_price = TARIFF_31
 daily_usage = int(input("Enter daily use in kWh: "))
 billing_period = int(input("Enter number of billing days: "))
 estimated_bill = hourly_price * daily_usage * billing_period
