2230d568524cde5fdad35099e5f3bea148299b15
Thomas-Wallis <thomas.walis@my.jcu.edu.au>
2020-08-24 11:25:38 +1000

    Added broken_score program and altered code to allow for functions

---
+++ prac_03/broken_score.py
@@ -0,0 +1,19 @@
+def main():
+    score = float(input("Enter score: "))
+    result = score_checker(score)
+    print(result)
+
+
+def score_checker(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif 50 <= score < 90:
+        result = "Passable"
+    elif score >= 90:
+        result = "Excellent"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
