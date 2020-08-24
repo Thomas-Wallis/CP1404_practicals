0aa710fc0d9e854469d506b506fb5ef54dd45449
Thomas-Wallis <thomas.walis@my.jcu.edu.au>
2020-08-24 12:18:22 +1000

    Checked answers and added correct answers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,4 +1,6 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Assumed answers
 # numbers[0] = 3
 # numbers[-1] = 2
 # numbers[3] = 1
@@ -8,3 +10,20 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers = 2
 # "3" in numbers = 3
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Actual answers if wrong
+# numbers[:-1] = [3, 1, 4, 1, 5, 9]
+# numbers[3:4] = [1]
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers = False
+
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
