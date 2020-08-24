3e1ca5c0d3f90c526e09e652bf829b4c54f71d0a
Thomas-Wallis <thomas.walis@my.jcu.edu.au>
2020-08-24 16:26:33 +1000

    Removed redundant information

--- prac_01/prac_01_practice/menus.py
+++ prac_01/prac_01_practice/menus.py
@@ -3,11 +3,9 @@ menu = input("H - Hello\nG - Goodbye\nQ - Quit\nPlease choose from the options a
 while menu.upper() != "Q":
     if menu.upper() == "H":
         print("Hello {}".format(name))
-        menu = input("H - Hello\nG - Goodbye\nQ - Quit\nPlease choose from the options above: ")
     elif menu.upper() == "G":
         print("Goodbye {}".format(name))
-        menu = input("H - Hello\nG - Goodbye\nQ - Quit\nPlease choose from the options above: ")
     else:
         print("Invalid")
-        menu = input("H - Hello\nG - Goodbye\nQ - Quit\nPlease choose from the options above: ")
+    menu = input("H - Hello\nG - Goodbye\nQ - Quit\nPlease choose from the options above: ")
 print("Finished")
