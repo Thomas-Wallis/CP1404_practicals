64ae5ee059b63a77f5da6273241b3242577c6428
Thomas-Wallis <thomas.walis@my.jcu.edu.au>
2020-08-31 09:38:00 +1000

    Added colour dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+hex_colours = {"aliceblue": "#f0f8ff", "antiquewhite": "faebd7", "antiquewhite1": "ffefdb", "antiquewhite2": "#eedfcc",
+               "antiquewhite3": "#cdc0b0", "antiquewhite4": "#8b8378", "aquamarine1": "#8b8378",
+               "aquamarine2": "#7fffd4", "aquamarine4": "#76eec6", "azure1": "#f0ffff", "azure2": "#e0eeee"}
