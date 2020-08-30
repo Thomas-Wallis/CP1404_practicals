726a3307db44fa50c9f137faf03206c6bf953e95
Thomas-Wallis <thomas.walis@my.jcu.edu.au>
2020-08-31 09:47:42 +1000

    Added option for user to check hexadecimal colour codes with error checking

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,8 @@
-hex_colours = {"aliceblue": "#f0f8ff", "antiquewhite": "faebd7", "antiquewhite1": "ffefdb", "antiquewhite2": "#eedfcc",
+HEX_COLOURS = {"aliceblue": "#f0f8ff", "antiquewhite": "faebd7", "antiquewhite1": "ffefdb", "antiquewhite2": "#eedfcc",
                "antiquewhite3": "#cdc0b0", "antiquewhite4": "#8b8378", "aquamarine1": "#8b8378",
                "aquamarine2": "#7fffd4", "aquamarine4": "#76eec6", "azure1": "#f0ffff", "azure2": "#e0eeee"}
+colour = input("Please enter the colour you would like to check the hexadecimal code of: ").lower()
+if colour in HEX_COLOURS:
+    print("Code - {}".format(HEX_COLOURS[colour]))
+else:
+    print("Invalid input")
