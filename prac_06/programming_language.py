a8faaeae77e326818dc9c76880fa48665472b1f6
Thomas-Wallis <thomas.walis@my.jcu.edu.au>
2020-09-16 09:44:02 +1000

    Completed code for languages program and programming_language class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,9 +1,13 @@
 class ProgrammingLanguage:
 
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
 
-    def __init__(self):
-        pass
-
+    def __str__(self):
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year)
 
     def is_dynamic(self):
-        pass
+        return self.typing == "Dynamic"
