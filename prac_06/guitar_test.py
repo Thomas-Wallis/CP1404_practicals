d23fe16df066d7a069f07492c8d0e4bca7e5f960
Thomas-Wallis <thomas.walis@my.jcu.edu.au>
2020-09-16 10:22:41 +1000

    Finished testing in guitar_test program

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,16 @@
+from prac_06.guitar import Guitar
+
+CURRENT_YEAR = 2020
+VINTAGE = 50
+
+def main():
+    guitar_1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another_guitar = Guitar("Another Guitar", 2013, 13009.25)
+    guitars = guitar_1, another_guitar
+    print("{} get_age() - Expected {}. Got {}".format(guitar_1.name, 98, guitar_1.get_age()))
+    print("{} is_vintage() - Expected {}. Got {}".format(guitar_1.name, True, guitar_1.is_vintage()))
+    print("{} get_age() - Expected {}. Got {}".format(another_guitar.name, 7, another_guitar.get_age()))
+    print("{} is_vintage() - Expected {}. Got {}".format(another_guitar.name, False, another_guitar.is_vintage()))
+
+
+main()
