e4951e94a57bb95a4145f6b35a22fe50897a60a6
Thomas-Wallis <thomas.wallis@my.jcu.edu.au>
2020-10-06 16:34:30 +1000

    Used solutions to fix issues with own code.

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,20 +1,44 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
+from kivy.properties import StringProperty
+
+FACTOR_MILES_TO_KM = 1.60934
 
 
-class MilesToKilometresApp(App):
-    """ MilesToKilometresApp is a Kivy App for squaring a number """
+class MilesConverterApp(App):
+    """Kivy App for converting miles to kilometres."""
+    output_km = StringProperty()
+
     def build(self):
-        """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
-        self.title = "Miles To Kilometres"
+        """Build the Kivy app from the kv file."""
+        self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_conversion(self, value):
-        """ handles the conversion of miles to kilometres """
-        result = value * 1.609344
-        self.root.ids.output_label.text = str(result)
+    def handle_calculate(self, text):
+        """Handle calculation (could be button press or other call)."""
+        print("handle calculate")
+        miles = self.convert_to_number(text)
+        self.update_result(miles)
 
-MilesToKilometresApp().run()
+    def handle_increment(self, text, change):
+        """Handle up/down button press, update the text input with new value, call calculation function."""
+        print("handle increment")
+        miles = self.convert_to_number(text) + change
+        self.root.ids.input_miles.text = str(miles)
+        # Since the InputText.text has changed, its on_text event will fire and handle_calculate will be called
+
+    def update_result(self, miles):
+        print("update")
+        self.output_km = str(miles * FACTOR_MILES_TO_KM)
+
+    @staticmethod
+    def convert_to_number(text):
+        """Convert text to float or 0.0 if invalid."""
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
+
+
+MilesConverterApp().run()
