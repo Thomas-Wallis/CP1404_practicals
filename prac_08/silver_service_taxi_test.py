b5ae5f5df15e7af13b2def495f57b0aeef0692cc
Thomas-Wallis <thomas.wallis@my.jcu.edu.au>
2020-10-17 16:35:36 +1000

    More minor formatting changes.

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -6,7 +6,7 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 
 def main():
-    taxi = SilverServiceTaxi("Test Silver Service Taxi", 100, 2)
+    taxi = SilverServiceTaxi("Hummer", 100, 2)
     taxi.drive(18)
     print(taxi)
     print(taxi.get_fare())
